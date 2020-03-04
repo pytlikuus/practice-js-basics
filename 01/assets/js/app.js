47440fc77a4f1817e57e10984eb9637c0320ce61
pytlikuus <kontomailowe1@o2.pl>
2020-03-04 22:11:18 +0100

    for loop and while loop

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,17 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+const typA = typeof (a);
+const typB = typeof (b);
+
+console.log(typA, typB)
+
+const c = parseFloat(a);
+
+const plus = c + b;
+const minus = c - b;
+const mnozenie = c * b;
+const dzielenie = c / b;
+
+console.log(plus, minus, mnozenie.toFixed(2), dzielenie.toFixed(2));
--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,28 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = prompt('Podaj liczbę!')
+const x = 3;
 
+if (x > 0 && x <= 10 && isNaN(x) !== true) {
 
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i} x ${x} = ${i * x}`)
+    }
+} else {
+    console.log('Podaj liczbe z przedzialu 1 - 10')
+}
 
 /* rozwiązanie z pętlą while  */
+
+const a = 7;
+const n = 2;
+let suma = a;
+let i = 1;
+let wynik = a;
+
+while (i < n) {
+    suma *= a;
+    i++;
+    wynik += ` * ${a}`
+
+}
+console.log(`${a} do potęgi ${n} wynosi: ${wynik} = ${suma}`);
